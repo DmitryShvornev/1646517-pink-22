abf37039302fa7f78610bd0396faca7c99107cff
shvor <shvornev.dmitry@yandex.ru>
2021-05-19 16:08:22 +0300

    Финальные улучшения

--- source/js/app.js
+++ source/js/app.js
@@ -5,6 +5,13 @@ var pageHeader = document.querySelector(".page-header");
 navigationMain.classList.remove("main-navigation--no-js");
 pageHeader.classList.remove("page-header--no-js");
 
+if (document.body.contains(document.querySelector(".page-footer__map-image"))) {
+  var mapImg = document.querySelector(".page-footer__map-image");
+  var mapDisplay = document.querySelector(".page-footer__map-display");
+  mapImg.classList.add("hidden");
+  mapDisplay.classList.remove("hidden");
+}
+
 navigationToggle.addEventListener("click", function() {
   if (navigationMain.classList.contains("main-navigation--closed")) {
     navigationMain.classList.remove("main-navigation--closed");
